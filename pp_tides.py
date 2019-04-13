94a0758f6c756cdfeefb59928de866e700a19f27
caternuson <caternuson@gmail.com>
2019-04-13 11:34:55 -0700

    updates

--- pp_tides.py
+++ pp_tides.py
@@ -4,11 +4,15 @@ from adafruit_pyportal import PyPortal
 from adafruit_bitmap_font import bitmap_font
 from adafruit_display_text.label import Label
 
-STATION_ID = "9447130" # find yours here: https://tidesandcurrents.noaa.gov/
+#--| USER CONFIG |--------------------------
+STATION_ID = "9447130"   # tide location, find yours here: https://tidesandcurrents.noaa.gov/
+HI_COLOR   = 0x00FF00    # high tide times color
+LO_COLOR   = 0x11FFFF    # low tide times color
+DATE_COLOR = 0xFFFFFF    # date and time color
+#-------------------------------------------
 
 DATA_SOURCE = "https://tidesandcurrents.noaa.gov/api/datagetter?date=today&product=predictions&datum=mllw&interval=hilo&format=json&units=metric&time_zone=lst_ldt&station="+STATION_ID
 DATA_LOCATION = ["predictions"]
-UPDATE_TIME = 3 # 24 hour based time, ex: 3 = 3AM
 
 # determine the current working directory needed so we know where to find files
 cwd = ("/"+__file__).rsplit('/', 1)[0]
@@ -20,61 +24,80 @@ pyportal = PyPortal(url=DATA_SOURCE,
 # Connect to the internet and get local time
 pyportal.get_local_time()
 
-# Tide info setup
+# Setup tide times font
 tide_font = bitmap_font.load_font(cwd+"/fonts/cq-mono-30.bdf")
 tide_font.load_glyphs(b'1234567890:')
-tide_colors =    { "L" : 0x11FFFF ,
-                   "H" : 0x00FF00 }
-tide_locations = { "L" : ((180, 80), (180, 165)) ,
-                   "H" : (( 40, 80), ( 40, 165)) }
 
-# Date info setup
+# Setup date and time font
 date_font = bitmap_font.load_font(cwd+"/fonts/Arial-12.bdf")
 date_font.load_glyphs(b'1234567890-')
-date_color = 0xFFFFFF
-date_location = (115, 228)
 
 # Labels setup
-HI_LABELS = [ Label(tide_font, text="00:00", color=tide_colors["H"], x=tide_locations["H"][0][0], y=tide_locations["H"][0][1]) ,
-              Label(tide_font, text="00:00", color=tide_colors["H"], x=tide_locations["H"][1][0], y=tide_locations["H"][1][1]) ]
-LO_LABELS = [ Label(tide_font, text="00:00", color=tide_colors["L"], x=tide_locations["L"][0][0], y=tide_locations["L"][0][1]) ,
-              Label(tide_font, text="00:00", color=tide_colors["L"], x=tide_locations["L"][1][0], y=tide_locations["L"][1][1]) ]
-DATE_LABEL = Label(date_font, text="0000-00-00", color=date_color, x=date_location[0], y=date_location[1])
-labels = HI_LABELS + LO_LABELS + [DATE_LABEL]
+HI_LABELS = [ Label(tide_font, text="00:00", color=HI_COLOR, x= 40, y= 80) ,
+              Label(tide_font, text="00:00", color=HI_COLOR, x= 40, y=165) ]
+LO_LABELS = [ Label(tide_font, text="00:00", color=LO_COLOR, x=180, y= 80) ,
+              Label(tide_font, text="00:00", color=LO_COLOR, x=180, y=165) ]
+DATE_LABEL = Label(date_font, text="0000-00-00 00:00:00", color=DATE_COLOR, x=75, y=228)
 
 # Add all the labels to the display
-for label in labels:
+for label in HI_LABELS + LO_LABELS + [DATE_LABEL]:
     pyportal.splash.append(label)
 
-def update_display():
-    # Fetch data from NOAA
-    tide_info = pyportal.fetch()
+def get_tide_info():
+    """Fetch JSON tide time info and return it."""
 
-    # Clear current info
-    for label in labels:
-        label.text = ""
+    # Get raw JSON data
+    raw_info = pyportal.fetch()
 
-    # Extract high/low times and update labels
-    h = l = 0
-    for tide in tide_info:
-        tide_type = tide["type"]
-        tide_time = tide["t"].split(" ")[1]
-        if tide_type == "H":
-            HI_LABELS[h].text = tide_time
-            h += 1
-        if tide_type == "L":
-            LO_LABELS[l].text = tide_time
-            l += 1
+    # Return will be a dictionary of lists containing tide times
+    tide_info = {"H":[], "L":[]}
 
-    # Update date label
-    DATE_LABEL.text = tide_info[0]["t"].split(" ")[0]
+    # Parse out the tide time info
+    for info in raw_info:
+        tide_type = info["type"]
+        tide_time = info["t"].split(" ")[1]
+        tide_info[tide_type].append(tide_time)
+
+    return tide_info
+
+def update_display(current_time, update_tides=False):
+    """Update the display with current info."""
+
+    # Tide time info
+    if update_tides:
+        # out with the old
+        for label in HI_LABELS + LO_LABELS:
+            label.text = ""
+        # in with the new
+        for i, hi_time in enumerate(tide_info["H"]):
+            HI_LABELS[i].text = hi_time
+        for i, lo_time in enumerate(tide_info["L"]):
+            LO_LABELS[i].text = lo_time
+
+    # Date and time
+    DATE_LABEL.text = "{:04}-{:02}-{:02} {:02}:{:02}:{:02}".format(current_time.tm_year,
+                                                                   current_time.tm_mon,
+                                                                   current_time.tm_mday,
+                                                                   current_time.tm_hour,
+                                                                   current_time.tm_min,
+                                                                   current_time.tm_sec)
+
+    board.DISPLAY.refresh_soon()
 
 # First run update
-update_display()
+tide_info = get_tide_info()
+current_time = time.localtime()
+update_display(current_time, True)
+current_yday = current_time.tm_yday
 
 # Update daily
 while True:
-    if time.localtime().tm_hour == UPDATE_TIME:
-        update_display()
-        time.sleep(23 * 60 * 60)
-    time.sleep(300)
+    current_time = time.localtime()
+    new_tides = False
+    if current_time.tm_yday != current_yday:
+        # new day, time to update
+        tide_info = get_tide_info()
+        new_tides = True
+        current_yday = current_time.tm_yday
+    update_display(current_time, new_tides)
+    time.sleep(0.5)
